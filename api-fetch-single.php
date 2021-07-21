20abf8c37c3c54edf2d7c836444b4f77b635c87c
jibrankhalil <dev.jibrankhalil@gmail.com>
2021-07-21 14:27:23 +0500

    first commit

---
+++ api-fetch-single.php
@@ -0,0 +1,19 @@
+<?php 
+
+// header('Content-Type : application/json');
+header('Acess-Control-Allow-Origin: *');
+include "config.php";
+
+$sql = "SELECT * FROM students";
+$results = mysqli_query($conn, $sql) or die("SQL query failed");
+
+if(mysqli_num_rows($results) > 0 ){
+$output = mysqli_fetch_all($results, MYSQLI_ASSOC);
+echo json_encode($output); 
+}else{
+    echo json_encode(array('message'=> 'No Record Found', 'status'=> false)); 
+
+}
+
+
+?>
---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "api";
+
+
+$conn = mysqli_connect($hostname,$username,$password,$dbname) or die("Error db not connected");
+
+
+?>
